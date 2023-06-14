7c3bd5976699c3e9c655ba7bd80a30bdb5b756ce
JablonskaAnna <jablonskanna654@gmail.com>
2023-06-14 20:19:58 +0200

    Blok 10

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/SelectorsAndCascade.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style.css';
+
+export function SelectorsAndCascade() {
+  return (
+    <div>
+      <div className="mydiv-1">Jestem bardzo unikalnym elementem
+      <p>Paragraf 1</p></div>
+      <div className="mydiv-2">Jestem bardzo unikalnym elementem 2</div>
+      <div className="mydiv-3">Jestem bardzo unikalnym elementem3<span>Abcd 3.1 ab</span></div>
+      <div className="mydiv-4"><p>Jestem bardzo unikalnym elementem4<p>Dlatego jestem ważny</p></p>
+      <p>Jestem bardzo unikalnym elementem<p>Dlatego jestem ważny</p></p>
+      </div>
+      <div>Jestem bardzo unikalnym elementem</div>
+      <div>Jestem bardzo unikalnym elementem</div>
+    </div>
+  );
+}
